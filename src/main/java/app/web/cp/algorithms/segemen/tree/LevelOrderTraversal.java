d460079ed9f2ca593558ec85140c55cf4a439302
Shashank <shashaanktyagi@gmail.com>
2024-04-29 06:45:33 +0530

    Segment Tree

---
+++ src/main/java/app/web/cp/algorithms/segemen/tree/LevelOrderTraversal.java
@@ -0,0 +1,18 @@
+package app.web.cp.algorithms.segemen.tree;
+
+public class LevelOrderTraversal implements Traversal {
+    @Override
+    public int left(int parent, int left, int right) {
+        return 2 * parent;
+    }
+
+    @Override
+    public int right(int parent, int left, int right) {
+        return 2 * parent + 1;
+    }
+
+    @Override
+    public int total(int n) {
+        return 4 * n;
+    }
+}
---
+++ src/main/java/app/web/cp/algorithms/segemen/tree/Main.java
@@ -0,0 +1,7 @@
+package app.web.cp.algorithms.segemen.tree;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
