a097fb5b782de63f307e334e8c2fe2f0b9f5a5b9
Shashank <shashaanktyagi@gmail.com>
2024-04-30 05:31:34 +0530

    Added Pre order traversal

---
+++ src/main/java/app/web/cp/algorithms/segemen/tree/PreOrderTraversal.java
@@ -0,0 +1,19 @@
+package app.web.cp.algorithms.segemen.tree;
+
+public class PreOrderTraversal implements Traversal {
+    @Override
+    public int left(int parent, int left, int right) {
+        return  parent  + 1;
+    }
+
+    @Override
+    public int right(int parent, int left, int right) {
+        int mid = left + (right - left) /2;
+        return parent + 2 * (mid - left + 1);
+    }
+
+    @Override
+    public int total(int n) {
+        return 2 * n;
+    }
+}
